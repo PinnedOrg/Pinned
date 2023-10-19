c4f08690947cee761c937b2329135a4f64a06248
HazemS24 <hazem.saad2004@gmail.com>
2023-10-19 00:13:16 -0400

    Header changes - search bar left

--- pinned-react/src/App.js
+++ pinned-react/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { LuFilter } from 'react-icons/lu';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Pinned, the beginning
+        <h1 className="title">Pinned</h1>
+        <h1 className="filter"><LuFilter /></h1>
       </header>
     </div>
   );
