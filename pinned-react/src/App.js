0e3e053fbd2906e64f403eb6688b44685993587a
HazemS24 <hazem.saad2004@gmail.com>
2023-10-19 23:37:34 -0400

    Style fixes

--- pinned-react/src/App.js
+++ pinned-react/src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import { LuFilter } from 'react-icons/lu';
+import logo from "./logo.svg";
+import "./App.css";
+import { LuFilter } from "react-icons/lu";
+import { BiSearchAlt } from "react-icons/bi";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Pinned</h1>
-        <h1 className="filter"><LuFilter /></h1>
+        <div className="right_header">
+          <div className="search_function">
+            <div id="search_input" className="search_bar">
+              <input type="search" placeholder="Search" id="form_one" className="search_bar" />
+            </div>
+            <button type="button" className="button">
+              <BiSearchAlt />
+            </button>
+          </div>
+          <h1 className="filter">
+            <LuFilter />
+          </h1>
+        </div>
       </header>
     </div>
   );
