51e62b08460e00353c7c3600ee17df97be31a2b0
eshaan-mehta <eshaanm.mehta@gmail.com>
2023-10-27 17:56:34 -0400

    "reset"

--- pinned-react/src/pages/bulletin/components/side-menu.js
+++ pinned-react/src/pages/bulletin/components/side-menu.js
@@ -1,7 +1,4 @@
-import './side-menu.css';
-
 import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button';
 
 import { HiPlus } from "react-icons/hi"
 
@@ -9,20 +6,8 @@ const SideMenu = () => {
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false)
 
   return (
-    <div className='plus-button-container' onClick={()=> setIsSideMenuOpen(!isSideMenuOpen)}>
-      {!isSideMenuOpen ?
-         (<Button className='plus-button'>
-         <span>
- 
-           <HiPlus />
-         </span>
-       </Button>) : (
-        <nav className='side-menu'>
-
-        </nav>
-       )
+    <div className='' onClick={()=> setIsSideMenuOpen(!isSideMenuOpen)}>
       
-      }
      
     </div>
   )
