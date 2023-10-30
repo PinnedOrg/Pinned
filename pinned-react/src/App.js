bedc2d94dfc739ab7e9c35f9c5ad706b13f3b0ec
eshaan-mehta <eshaanm.mehta@gmail.com>
2023-10-29 21:17:16 -0400

    "for main"

--- pinned-react/src/App.js
+++ pinned-react/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import SideMenu from './pages/bulletin/components/side-menu';
+
 
 function App() {
   return (
     <div className="h-screen w-screen bg-gray-50 text-gray-950 dark:[#282c34]">
       <header className="">
       
-        <SideMenu />  
+          
       </header>
     </div>
 
