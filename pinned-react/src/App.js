680376257e8481fac08b1e1cc7802603c119c88e
eshaan-mehta <eshaanm.mehta@gmail.com>
2023-10-29 20:49:28 -0400

    "new logo"

--- pinned-react/src/App.js
+++ pinned-react/src/App.js
@@ -3,7 +3,7 @@ import SideMenu from './pages/bulletin/components/side-menu';
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-gray-50 dark:[#282c34]">
+    <div className="h-screen w-screen bg-gray-50 text-gray-950 dark:[#282c34]">
       <header className="">
       
         <SideMenu />  
--- pinned-react/src/pages/bulletin/components/side-menu.js
+++ pinned-react/src/pages/bulletin/components/side-menu.js
@@ -10,7 +10,7 @@ const SideMenu = () => {
     <section className="relative h-screen">
       {isSideMenuOpen &&  
       <div 
-        className="absolute h-full w-[20rem] bg-none border-[0.13rem] border-actionOrange right-0 top-0 items-start justify-center text-gray-50">
+        className="absolute h-full w-[20rem] bg-none border-[0.13rem] border-actionOrange right-0 top-0 items-start justify-center">
         
         
         <button className=" text-[1.5rem] mt-1 ml-1 text-gray-500 hover:scale-[1.1] active:scale-105 transition hover:text-gray-800" onClick={() => setIsSideMenuOpen(false)}>
@@ -20,7 +20,8 @@ const SideMenu = () => {
         
       </div>}
 
-      {!isSideMenuOpen && <button className='fixed bottom-0 right-0 mr-10 mb-8 p-2 text-[2rem] rounded-full bg-actionOrange text-white hover:scale-105' onClick={() => setIsSideMenuOpen(true)}>
+      {!isSideMenuOpen && 
+      <button className='fixed bottom-0 right-0 mr-10 mb-8 p-2 text-[2rem] rounded-full bg-actionOrange text-white hover:scale-110 active:scale-105' onClick={() => setIsSideMenuOpen(true)}>
         <HiPlus />
       </button>}
 
