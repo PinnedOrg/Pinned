b8102518d1f7718fb479dfcecf2c3b026c3861fa
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2023-11-12 17:04:19 -0500

    Added Routes

--- pinned-react/src/App.js
+++ pinned-react/src/App.js
@@ -1,17 +1,24 @@
 import NewEventSystem from './pages/components/bulletin/new event/new-event-system';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from './pages/LandingPage/LandingPage.js'
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-gray-50 text-gray-950 dark:[#282c34]">
-      <header className="flex">
-          <h1 className='text-center border w-full border-black bg-actionOrange h-[3rem]'>
-            Temp Heading
-          </h1>
-          <NewEventSystem />
-      </header>
-    </div>
-
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<LandingPage/>}/>
+        <Route path="/NewEventPage" element={   
+           <div className="h-screen w-screen bg-gray-50 text-gray-950 dark:[#282c34]">
+            <header className="flex">
+                <h1 className='text-center border w-full border-black bg-actionOrange h-[3rem]'>
+                  Temp Heading
+                </h1>
+                <NewEventSystem />
+            </header>
+          </div>
+        }/>
+      </Routes>
+    </BrowserRouter>
     
   );
 }
