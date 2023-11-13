c1a8d701c7905da4be5924caf630f2b71f4faa54
HazemS24 <hazem.saad2004@gmail.com>
2023-11-13 18:54:25 -0500

    Created Header

--- pinned-react/src/App.js
+++ pinned-react/src/App.js
@@ -1,21 +1,26 @@
-import NewEventSystem from './pages/components/bulletin/new event/new-event-system';
+import NewEventSystem from "./pages/components/bulletin/new event/new-event-system";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from './pages/LandingPage/LandingPage.js';
-import MainPage from './pages/MainPage/MainPage.js';
+import LandingPage from "./pages/LandingPage/LandingPage.js";
+import NavBar from "./pages/NavBar/NavBar.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<LandingPage/>}/>
-        <Route path="/NewEventPage" element={   
-           <div className="h-screen w-screen bg-gray-50 text-gray-950 dark:[#282c34]">
-            <MainPage />
-          </div>
-        }/>
+        <Route index path="/" element={<LandingPage />} />
+        <Route
+          path="/NewEventPage"
+          element={
+            <div className="h-screen bg-gray-50 text-gray-950 dark:[#282c34]">
+              <NavBar />
+              <header className="flex">
+                <NewEventSystem />
+              </header>
+            </div>
+          }
+        />
       </Routes>
     </BrowserRouter>
-    
   );
 }
 
