944bcc30804a2bcfaf41ef72a089bc9075c7a117
eshaan-mehta <eshaanm.mehta@gmail.com>
2023-11-13 08:41:36 -0500

    reordered stuff

--- pinned-react/src/index.js
+++ pinned-react/src/index.js
@@ -9,9 +9,7 @@ import { PrimeReactProvider } from 'primereact/api';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PrimeReactProvider value={{ unstyled: true, pt: { }}} >
       <App />
-    </PrimeReactProvider>
   </React.StrictMode>
 );
 
