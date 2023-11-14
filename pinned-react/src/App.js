49beda4d69923f71e07972843299bd51e0803c1d
HazemS24 <73613050+HazemS24@users.noreply.github.com>
2023-11-13 20:40:10 -0500

    Create header (#9)
    * Initial Commit with new file components
    
    * Created Header
    
    * Fixed height issue with plus symbol
    
    * Fixed Typos

--- pinned-react/src/App.js
+++ pinned-react/src/App.js
@@ -1,25 +1,26 @@
-import NewEventSystem from './pages/components/bulletin/new event/new-event-system';
+import NewEventSystem from "./pages/components/bulletin/new event/new-event-system";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from './pages/LandingPage/LandingPage.js'
+import LandingPage from "./pages/LandingPage/LandingPage.js";
+import NavBar from "./pages/NavBar/NavBar.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<LandingPage/>}/>
-        <Route path="/NewEventPage" element={   
-           <div className="h-screen w-screen bg-gray-50 text-gray-950 dark:[#282c34]">
-            <header className="flex">
-                <h1 className='text-center border w-full border-black bg-actionOrange h-[3rem]'>
-                  Temp Heading
-                </h1>
+        <Route index path="/" element={<LandingPage />} />
+        <Route
+          path="/NewEventPage"
+          element={
+            <div className="h-screen bg-gray-50 text-gray-950 dark:[#282c34]">
+              <NavBar />
+              <header className="flex">
                 <NewEventSystem />
-            </header>
-          </div>
-        }/>
+              </header>
+            </div>
+          }
+        />
       </Routes>
     </BrowserRouter>
-    
   );
 }
 
