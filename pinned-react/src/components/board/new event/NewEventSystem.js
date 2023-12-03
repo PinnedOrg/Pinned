d74b6393889959f0dd8435065af3203e99dc94cc
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2023-12-03 17:31:36 -0500

    axios setup (#19)
    * Merge Backend
    
    * Added methods to event controller
    
    * Fixed
    
    * Commented code and completed event controller
    
    * Quick fixes
    
    * started board model
    
    * Organization fixes
    
    * get single board function for controller
    
    * added board controller, updated events controller
    
    * added comments
    
    * added board comments
    
    * fixed create event bug
    
    * axios setup
    
    ---------
    
    Co-authored-by: HazemS24 <hazem.saad2004@gmail.com>
    Co-authored-by: eshaan-mehta <eshaanm.mehta@gmail.com>

--- pinned-react/src/components/board/new event/NewEventSystem.js
+++ pinned-react/src/components/board/new event/NewEventSystem.js
@@ -1,9 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PlusButton from './PlusButton'
 import EditEventMenu from '../edit event menu/edit-event-menu'
+import axios from 'axios'
 
 const NewEventSystem = () => {
   const [isEditEventMenuOpen, setIsEditEventMenuOpen] = useState(false)
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/events');
+        setData(response.data);
+        console.log('Data has been received!');
+        console.log(response)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="relative">
