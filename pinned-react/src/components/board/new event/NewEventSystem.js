77d575f2a36547ff411c1795329753ac4b99e434
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2023-12-10 19:50:32 -0500

    create new events

--- pinned-react/src/components/board/new event/NewEventSystem.js
+++ pinned-react/src/components/board/new event/NewEventSystem.js
@@ -11,7 +11,7 @@ const NewEventSystem = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/api/events');
+        const response = await axios.get('http://localhost:8000/api/events');
         setData(response.data);
         console.log('Data has been received!');
         console.log(response)
