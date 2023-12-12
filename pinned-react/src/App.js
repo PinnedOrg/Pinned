1b32bb2709aa1bc76f84022c98bb6948d74ec370
eshaan-mehta <114041487+eshaan-mehta@users.noreply.github.com>
2023-12-11 19:33:21 -0500

    Create new board (#23)
    * fixed routing issue. Board pulls from backend
    
    * new comment
    
    * update event

--- pinned-react/src/App.js
+++ pinned-react/src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage/LandingPage.js'
 import BoardHome from './pages/Board/BoardHome.js';
-import GeneralPage from './pages/Board/GeneralPage.js';
-import EventsPage from './pages/Board/EventsPage.js';
+import GeneralPage from "./pages/Board/GeneralPage.js";
+import EventsPage from "./pages/Board/EventsPage.js";
+import CalendarPage from "./pages/CalendarPage.js";
 
-
-function App() {
-  const boardRoute = 123874612 //will be fetched from backend
+const App = () => {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<LandingPage/>}/>    
-        <Route path={`board:${boardRoute}`} element={<BoardHome />}> 
-          <Route path='general' element={<GeneralPage/>}/>
-          <Route id='eventroute' path='events' element={<EventsPage />} />
-          <Route path='calendar' element/>
+        <Route index element={<LandingPage/>} />
+
+        <Route path="/board/:id">
+          <Route index element={<BoardHome />}/> {/* <------- This line right here fixes the nested route rendering issue */}
+          <Route path="general" element={<GeneralPage />} />
+          <Route path="events" element={<EventsPage />} />
+          <Route path="calendar" element={<CalendarPage />} />
         </Route>
+
       </Routes>
     </BrowserRouter>
-    
-    
   );
 }
 
--- pinned-react/src/pages/Board/EventsPage.js
+++ pinned-react/src/pages/Board/EventsPage.js
@@ -4,12 +4,12 @@ import NewEventSystem from '../../components/board/new event/NewEventSystem'
 const EventsPage = () => {
   return (
     <div className="h-screen w-screen bg-gray-50 text-gray-950 dark:[#282c34]">
-        <header className="flex">
+        
             <h1 className='text-center border w-full border-black bg-actionOrange h-[3rem]'>
                 Temp Heading
             </h1>
             <NewEventSystem />
-        </header>
+        
     </div>
   )
 }
