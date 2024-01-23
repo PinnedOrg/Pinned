76936a2cee0d82992e81af1b2691c65cbde8137b
HazemS24 <hazem.saad2004@gmail.com>
2024-01-23 17:18:12 -0500

    Created routes for dashboard

--- pinned-react/src/App.tsx
+++ pinned-react/src/App.tsx
@@ -1,35 +1,48 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SignedOut, SignedIn } from "@clerk/clerk-react"
+
 import { BoardContextProvider } from '@/context/BoardContext';
+
 import LandingPage from '@/pages/LandingPage/LandingPage'
 import BoardHome from '@/pages/Board/BoardHome';
 import GeneralPage from "@/pages/Board/GeneralPage";
 import EventsPage from "@/pages/Board/EventsPage";
 import CalendarPage from "@/pages/Board/CalendarPage";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App = () => {
 
   return (
-    
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<LandingPage/>} />
+  <div>
 
-        <Route 
-          path="/board/:id/*" 
-          element={
-            <BoardContextProvider>
-              <Routes>
-                <Route index element={<BoardHome />} />
-                <Route path="general" element={<GeneralPage />} />
-                <Route path="events" element={<EventsPage />} />
-                <Route path="calendar" element={<CalendarPage />} />
-              </Routes>
-            </BoardContextProvider>
-          }>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<LandingPage/>} />
+
+      <Route 
+        path="/board/:id/*" 
+        element={
+          <BoardContextProvider>
+            <Routes>
+              <Route index element={<BoardHome />} />
+              <Route path="general" element={<GeneralPage />} />
+              <Route path="events" element={<EventsPage />} />
+              <Route path="calendar" element={<CalendarPage />} />
+            </Routes>
+          </BoardContextProvider>
+        }>
+      </Route>
+      <SignedOut>
+        <p>This content is public. Only signed out users can see this.</p>
+      </SignedOut>
+      <SignedIn>
+        <p>This content is private. Only signed in users can see this.</p>
+        <Route path="dashboard" element={<Dashboard/>}/>
+      </SignedIn>
+    </Routes>
+  </BrowserRouter>
+  
+  </div>
   );
 }
 
