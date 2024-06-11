0ebc6167a364c63b19d4e2671de4b83b208e5ca7
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2024-06-11 18:09:37 -0400

    Profile banner

--- pinned-react/src/pages/TestPage - Hazem/TestPage.tsx
+++ pinned-react/src/pages/TestPage - Hazem/TestPage.tsx
@@ -1,10 +1,37 @@
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
 const TestPage = () => {
   return (
-    <div>
-      Test Page for Hazem
+    <div className="relative w-full">
+      <div className="relative w-full h-[30vh] rounded-lg overflow-hidden">
+      <img src="https://cdn.evbstatic.com/s3-build/fe/build/images/2e27983556c064883a94ddb9bffafcc2-toronto_desktop.webp" alt="Banner" className="w-full h-full object-cover" />
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black to-transparent flex flex-col justify-center px-8 text-white">
+          <h1 className="text-4xl">Best Club to Join</h1>
+          <p className="mt-2 text-sm">
+            Join our amazing club and be a part of a vibrant community. Enjoy exclusive events, make new friends, and have a great time!
+          </p>
+          <div className="flex space-x-4 mt-4">
+            <Avatar>
+              <AvatarImage src="path-to-facebook-icon.png" alt="Facebook" /> 
+              <AvatarFallback>FB</AvatarFallback>
+            </Avatar>
+            <Avatar>
+              <AvatarImage src="path-to-instagram-icon.png" alt="Instagram" /> 
+              <AvatarFallback>IG</AvatarFallback>
+            </Avatar>
+            <Avatar>
+              <AvatarImage src="path-to-email-icon.png" alt="Email" />
+              <AvatarFallback>Email</AvatarFallback>
+            </Avatar>
+            <Avatar>
+              <AvatarImage src="path-to-website-icon.png" alt="Website" />
+              <AvatarFallback>Web</AvatarFallback>
+            </Avatar>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default TestPage;
