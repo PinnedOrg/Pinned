b071c6b47c873aeed04aa451744fb1d2dac5e312
eshaan-mehta <eshaanm.mehta@gmail.com>
2024-06-26 16:06:45 -0400

    reorganized files. Added error message

---
+++ pinned-react/src/components/landing page/team.tsx
@@ -0,0 +1,19 @@
+import { team } from "@/lib/data";
+import { AboutCardType } from "@/lib/types";
+import AboutCard from "@/components/cards/AboutCard";
+const OurTeam = () => {
+  return (
+    <section className="container flex flex-wrap justify-center animate-fade-in-up">
+      <h1 className="w-full text-3xl font-bold text-center">Our Team</h1>
+      <div className="grid gap-8 mt-6 md:gap-10 xl:gap-6 md:grid-cols-2 xl:grid-cols-4">
+        {
+            team.map((member: AboutCardType) => (
+                <AboutCard {...member} key={member.name}/>
+            ))
+        }
+      </div>
+    </section>
+  )
+}
+
+export default OurTeam;
--- pinned-react/src/pages/ClubHub.tsx
+++ pinned-react/src/pages/ClubHub.tsx
@@ -166,6 +166,11 @@ const ClubHub = () => {
             ) : (
               <ClubNotFoundErrorMessage />
             )}
+            {isError ? (
+              <PageErrorMessage />
+            ) : (
+              <ClubNotFoundErrorMessage />
+            )}
           </div>
         ) : (
           <div className={clsx("grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4", {"gap-6": data})}>
