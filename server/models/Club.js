e496d8ba820121504968d74a8298b7b30755dbda
HazemS24 <73613050+HazemS24@users.noreply.github.com>
2024-06-26 22:30:09 -0400

    Add validation to clubs (#42)
    * commits
    
    * Added validation and filter in the front & back end. (set this to false for now for clubs to show up but it works)
    
    * Fixed validation

--- server/models/Club.js
+++ server/models/Club.js
@@ -75,6 +75,10 @@ const clubSchema = new mongoose.Schema(
         required: false,
         trim: true
     },
+    validation: {
+        type: Boolean,
+        default: false
+    },
     events: {
         type: [mongoose.Schema.Types.ObjectId],
         required: false,
