a89bceabb585ac5541e8a0cebb945589947bd81e
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2024-07-01 14:11:44 -0400

    update scroll positions

--- pinned-react/src/pages/clubprofile/ClubProfile.tsx
+++ pinned-react/src/pages/clubprofile/ClubProfile.tsx
@@ -12,10 +12,10 @@ const ClubProfile = () => {
       if (scrollPosition > 200) {
         setHasScrolledAboutUs(true);
       }
-      if (scrollPosition > 600) {
+      if (scrollPosition > 650) {
         setHasScrolledFacts(true);
       }
-      if (scrollPosition > 1000) {
+      if (scrollPosition > 1200) {
         setHasScrolledPhotos(true);
       }
     };
@@ -42,7 +42,7 @@ const ClubProfile = () => {
         <div className="shadow-purple-3"></div>
         <div className="shadow-blue-4"></div>
         <div className="shadow-purple-4"></div>
-        
+
         <div
           className={`relative -top-16 left-64 z-10 flex flex-col justify-center p-6 w-1/2 transition-transform duration-1000 ${
             isVisible ? 'transform translate-y-0' : 'transform translate-y-full'
