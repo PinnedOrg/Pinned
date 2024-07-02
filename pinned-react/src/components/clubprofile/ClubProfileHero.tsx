9f3d2e1f6de9865d749681ff4580f71b47e62a57
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2024-07-01 22:15:35 -0400

    refactor

---
+++ pinned-react/src/components/clubprofile/ClubProfileHero.tsx
@@ -0,0 +1,32 @@
+const ClubProfileHero = ({ isVisible }) => {
+    return (
+      <div
+        className={`relative -top-16 left-64 z-10 flex flex-col justify-center p-6 w-1/2 transition-transform duration-1000 ${
+          isVisible ? 'transform translate-y-0' : 'transform translate-y-full'
+        }`}
+      >
+        <h1 className="text-lg font-medium">WELCOME TO OUR CLUB</h1>
+        <h2 className="mt-2 text-7xl font-extrabold">JOIN THE NSBE</h2>
+        <p className="mt-6 text-lg max-w-md">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo adipiscing faucibus nunc amet convallis posuere diam nulla. Pellentesque vulputate dui posuere orci tellus dolor, semper convallis sed.
+        </p>
+        <button className="mt-6 w-48 px-6 py-2 bg-purple-600 text-white font-semibold rounded-full hover:bg-purple-700 transition duration-300">
+          Join Here
+        </button>
+        <div className="mt-4 flex space-x-4">
+          <a href="#" className="text-gray-400 hover:text-white">
+            FB
+          </a>
+          <a href="#" className="text-gray-400 hover:text-white">
+            IG
+          </a>
+          <a href="#" className="text-gray-400 hover:text-white">
+            TW
+          </a>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ClubProfileHero;
+  
---
+++ pinned-react/src/components/clubprofile/GradientBackground.tsx
@@ -0,0 +1,17 @@
+const GradientBackground = () => {
+    return (
+      <>
+        <div className="shadow-blue"></div>
+        <div className="shadow-purple"></div>
+        <div className="shadow-blue-2"></div>
+        <div className="shadow-purple-2"></div>
+        <div className="shadow-blue-3"></div>
+        <div className="shadow-purple-3"></div>
+        <div className="shadow-blue-4"></div>
+        <div className="shadow-purple-4"></div>
+      </>
+    );
+  };
+  
+  export default GradientBackground;
+  
