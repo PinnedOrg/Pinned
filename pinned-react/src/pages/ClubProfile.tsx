bbfe76231cb463d3957500b3c46cacc237377ce9
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2024-07-02 17:53:00 -0400

    Club page UI mvp (#44)
    * Added club cards and previews, fixed fetching from backend
    
    * added a low opacity version of the primary color
    
    * ability to run dev server on local network. "npm run local"
    
    * polishing and trying to connect the image saving to the club schema / controllers. Not working. Now displaying backend data instead of hardcoded data on club cards
    
    * added banner image to clubhub page, testing other colors, reduced navbar spacing
    
    * working on dark mode. navbar done
    
    * landing page
    
    * landing page + darkmode
    
    * fix
    
    * more updates to css
    
    * something tailwind config
    
    * readded FiltersType
    
    * navbar update
    
    * Profile banner
    
    * ClubForm
    
    * formatting + club title
    
    * card component
    
    * multiple events
    
    * form styling
    
    * FAQ accordion + formatting
    
    * landing page
    
    * Directory frontend (#38)
    
    * tailwind config fixes. Added primary hover color
    
    * created layout wrapper to have navbar on all pages
    
    * updated all pages to global routes
    
    * updated directory to clubhub. Created navbar
    
    * added register route
    
    * moved stuff around
    
    * navbar finished
    
    * clubhub
    
    * test page for hazem
    
    * Added club cards and previews, fixed fetching from backend
    
    * added a low opacity version of the primary color
    
    * ability to run dev server on local network. "npm run local"
    
    * polishing and trying to connect the image saving to the club schema / controllers. Not working. Now displaying backend data instead of hardcoded data on club cards
    
    * added banner image to clubhub page, testing other colors, reduced navbar spacing
    
    * working on dark mode. navbar done
    
    ---------
    
    Co-authored-by: eshaan-mehta <eshaanm.mehta@gmail.com>
    
    * landing page + darkmode
    
    * fix
    
    * something tailwind config
    
    * remove border, ig
    
    * pills
    
    * clubhub\
    
    * rename clubprofile from test
    
    * merge - use both testpage and clubprofile
    
    * club profile 2 mvp
    
    * styles
    
    * styles
    
    * about us
    
    * photo section
    
    * scroll controlled fade ins
    
    * facts section
    
    * glass morphism
    
    * background blurs
    
    * update scroll positions
    
    * Merge branch 'main' into club-page-ui-mvp
    
    * fixing clubprofile for the new file structure
    
    * refactor
    
    * refactor shadow
    
    * refactor ClubProfileItemizedDescription
    
    * refactor clubprofilephoto
    
    * club profile hero refactor
    
    * put back PUBLISHABLE_KEY
    
    ---------
    
    Co-authored-by: eshaan-mehta <eshaanm.mehta@gmail.com>
    Co-authored-by: Darius Rudaitis <dmruds123@gmail.com>

--- pinned-react/src/pages/ClubProfile.tsx
+++ pinned-react/src/pages/ClubProfile.tsx
@@ -1,100 +1,52 @@
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import { ClubForm } from "@/components/clubprofile/ClubForm";
-import { ClubEvent } from "@/components/clubprofile/ClubEvent";
-import { FAQAccordion } from "@/components/clubprofile/FAQAccordion";
-import { InstagramEmbed } from 'react-social-media-embed';
-import { Badge } from "@/components/ui/badge";
-import { FaUserFriends } from "react-icons/fa";
+import { useEffect, useState } from 'react';
+import GradientBackground from '@/components/clubprofile/GradientBackground';
+import ClubProfileHero from '@/components/clubprofile/ClubProfileHero';
+import ClubProfileAboutUs from '@/components/clubprofile/ClubProfileAboutUs';
+import ClubProfileItemizedDescription from '@/components/clubprofile/ClubProfileItemizedDescription';
+import ClubProfilePhotos from '@/components/clubprofile/ClubProfilePhotos';
+
 const ClubProfile = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [hasScrolledAboutUs, setHasScrolledAboutUs] = useState(false);
+  const [hasScrolledFacts, setHasScrolledFacts] = useState(false);
+  const [hasScrolledPhotos, setHasScrolledPhotos] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 200) {
+        setHasScrolledAboutUs(true);
+      }
+      if (scrollPosition > 650) {
+        setHasScrolledFacts(true);
+      }
+      if (scrollPosition > 1200) {
+        setHasScrolledPhotos(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
   return (
-    <div className="relative w-full">
-      <div className="relative w-full h-[30vh] rounded-lg overflow-hidden">
-      <img src="https://cdn.evbstatic.com/s3-build/fe/build/images/2e27983556c064883a94ddb9bffafcc2-toronto_desktop.webp" alt="Banner" className="w-full h-full object-cover" />
-        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black to-transparent flex flex-col justify-center px-8 text-white">
-          <h1 className="text-4xl">Best Club to Join</h1>
-          <p className="mt-2 text-sm">
-            Join our amazing club and be a part of a vibrant community. Enjoy exclusive events, make new friends, and have a great time!
-          </p>
-          <div className="flex space-x-4 mt-4">
-            <Avatar>
-              <AvatarImage src="path-to-facebook-icon.png" alt="Facebook" /> 
-              <AvatarFallback>FB</AvatarFallback>
-            </Avatar>
-            <Avatar>
-              <AvatarImage src="path-to-instagram-icon.png" alt="Instagram" /> 
-              <AvatarFallback>IG</AvatarFallback>
-            </Avatar>
-            <Avatar>
-              <AvatarImage src="path-to-email-icon.png" alt="Email" />
-              <AvatarFallback>Email</AvatarFallback>
-            </Avatar>
-            <Avatar>
-              <AvatarImage src="path-to-website-icon.png" alt="Website" />
-              <AvatarFallback>Web</AvatarFallback>
-            </Avatar>
-          </div>
-          <div className='flex w-full pt-20 space-x-8 sm:space-x-4 md:space-x-8 lg:space-x-4 xl:space-x-8'>
-            <Badge variant={'outline'} className='border-none text-zinc-500 bg-zinc-100 dark:text-gray-300 dark:bg-gray-800'>
-              Free
-            </Badge>
-            <Badge variant={'outline'} className='gap-2 border-none text-zinc-500 bg-zinc-100 dark:text-gray-300 dark:bg-gray-800'>
-              Over 100
-              <FaUserFriends />
-            </Badge>
-          </div>
-
-        </div>
+    <div>
+      <div className="relative min-h-screen text-white flex items-center">
+        <GradientBackground />
+        <ClubProfileHero isVisible={isVisible} />
       </div>
-      <div className="p-8 grid grid-cols-3 gap-4">
-        <div className="col-span-2 mt-4 px-4 flex flex-wrap gap-4">
-          <ClubEvent 
-              imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhkZ6g42MpLvGsAntEa-0keBi52LBsX8Y_Cw&s" 
-              title="sodfjsoifdj sdfsdofn" 
-              description="sdofu souvndbd ovdo dso di dfih ix ix ix ix vi vsi vis visbv hidvhdlh." 
-            />
-          <ClubEvent 
-            imageUrl="https://preview.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=640&crop=smart&auto=webp&s=22ed6cc79cba3013b84967f32726d087e539b699" 
-            title="sodfjsoifdj sdfsdofn" 
-            description="sdofu souvndbd ovdo dso di dfih ix ix ix ix vi vsi vis visbv hidvhdlh." 
-          />
-          <ClubEvent 
-            imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Kanye_West_at_the_2009_Tribeca_Film_Festival_%28crop_2%29.jpg/1200px-Kanye_West_at_the_2009_Tribeca_Film_Festival_%28crop_2%29.jpg" 
-            title="sodfjsoifdj sdfsdofn" 
-            description="sdofu souvndbd ovdo dso di dfih ix ix ix ix vi vsi vis visbv hidvhdlh." 
-          />
-                  <ClubEvent 
-            imageUrl="https://uwaterloo.ca/electrical-computer-engineering/sites/default/files/styles/large/public/uploads/images/harder_douglas.jpg?itok=OPtGC5j-" 
-            title="sodfjsoifdj sdfsdofn" 
-            description="sdofu souvndbd ovdo dso di dfih ix ix ix ix vi vsi vis visbv hidvhdlh." 
-          />
-          <ClubEvent 
-            imageUrl="https://static.vecteezy.com/system/resources/previews/022/149/130/non_2x/cute-white-paw-in-pixel-art-style-vector.jpg" 
-            title="sodfjsoifdj sdfsdofn" 
-            description="sdofu souvndbd ovdo dso di dfih ix ix ix ix vi vsi vis visbv hidvhdlh." 
-          />
-                  <ClubEvent 
-            imageUrl="https://media.cnn.com/api/v1/images/stellar/prod/190326162345-lead-justin-bieber-crop.jpg?q=w_3000,c_fill" 
-            title="sodfjsoifdj sdfsdofn" 
-            description="sdofu souvndbd ovdo dso di dfih ix ix ix ix vi vsi vis visbv hidvhdlh." 
-          />
-        </div>
-
-        <div className="col-span-1">
-          <ClubForm />
-        </div>
-      </div>
-      <div className="p-8 flex gap-4">
-        <div className="w-1/2">
-          <FAQAccordion />
-        </div>
-        <div className="w-1/2">
-          <InstagramEmbed url="https://www.instagram.com/p/CUbHfhpswxt/" width={328} />
-        </div>
-      </div>
-
-
+      <ClubProfileAboutUs hasScrolledAboutUs={hasScrolledAboutUs} />
+      <ClubProfileItemizedDescription hasScrolledFacts={hasScrolledFacts} />
+      <ClubProfilePhotos hasScrolledPhotos={hasScrolledPhotos} />
     </div>
   );
-}
+};
 
 export default ClubProfile;
