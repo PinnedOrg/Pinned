24d96b41aad995314df1dc27ae85c47406fa1ce4
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2024-07-01 22:24:28 -0400

    refactor ClubProfileItemizedDescription

---
+++ pinned-react/src/components/clubprofile/ClubProfileItem.tsx
@@ -0,0 +1,16 @@
+const ClubProfileItem = ({ number, title, description }) => {
+    return (
+      <div className="p-6 bg-gray-800 bg-opacity-20 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg">
+        <div className="flex items-center mb-4">
+          <div className="w-10 h-10 flex items-center justify-center bg-purple-600 text-white rounded-full font-bold">
+            {number}
+          </div>
+          <h4 className="ml-4 text-xl font-semibold">{title}</h4>
+        </div>
+        <p>{description}</p>
+      </div>
+    );
+  };
+  
+  export default ClubProfileItem;
+  
