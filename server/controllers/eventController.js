d197bf455468628853836dd3aabf9f327de26b3d
HazemS24 <73613050+HazemS24@users.noreply.github.com>
2024-07-13 13:55:28 -0400

    Added authentication, cleaned functionality and refactored naming conventions. (#52)

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -3,168 +3,179 @@ const Event = require("../models/Event");
 const Club = require("../models/Club");
 
 // get all events
-const getAllEvents = async (req, res) => {
-  // fetchs all events and sorts results in descending order
-  const events = await Event.find({}).sort({ createdAt: -1 }); //find is the criteria to search for (eg. title: "Event 1")
+const getEvents = async (req, res) => {
+	// fetchs all events and sorts results in descending order
+	const events = await Event.find({}).sort({ createdAt: -1 }); //find is the criteria to search for (eg. title: "Event 1")
 
-  res.status(200).json(events);
-};
-
-// get all the events associated with this club
-const getClubEventPreviews = async (req, res) => {
-  const { id } = req.params;
-
-  // if club id is invalid
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "Club not found." });
-  }
-
-  try {
-    // Find the club based on the provided ID
-    const club = await Club.findById(id);
-
-    if (!club) {
-      return res.status(404).json({ error: "Club not found." });
-    }
-    
-    // Find the club based on the provided ID
-    const events = await Event.find({ belongsToClub: id }).select("_id title description tags preview createdAt updatedAt").sort({ createdAt: -1 });
-
-    return res.status(200).json(events);
-  } catch (error) {
-    console.error("Error retrieving club events: ", error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
-
-  // get entire object
-  //const events = await Event.aggregate([{ $match: { belongsToClub: new mongoose.Types.ObjectId(id) }}]);
+	res.status(200).json(events);
 };
 
 // get a single event
 const getEvent = async (req, res) => {
-  // fetchs a single event based on id
-  const { id } = req.params;
+	// fetchs a single event based on id
+	const { id } = req.params;
 
-  // if event id is invalid
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "Event not found." });
-  }
+	// if event id is invalid
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "Event not found." });
+	}
 
-  const event = await Event.findById(id);
+	const event = await Event.findById(id);
 
-  // if event id does not exist
-  if (!event) {
-    return res.status(404).json({ error: "Event not found." });
-  }
+	// if event id does not exist
+	if (!event) {
+		return res.status(404).json({ error: "Event not found." });
+	}
 
-  res.status(200).json(event);
+	res.status(200).json(event);
 };
 
 // create an event
 const createEvent = async (req, res) => {
-  const {
-    title,
-    description,
-    contact,
-    tags,
-    date,
-    time,
-    location,
-    belongsToClub,
-  } = req.body;
+	const {
+		title,
+		description,
+		contact,
+		tags,
+		date,
+		time,
+		location,
+		belongsToClub,
+	} = req.body;
 
-  // checks if the club id is valid
-  if (!mongoose.Types.ObjectId.isValid(belongsToClub)) {return res.status(404).json({ error: "Club not found." });}
-  const club = await Club.findById(belongsToClub);
-  if (!club) {return res.status(404).json({ error: "Club not found." });}
+	// checks if the club id is valid
+	if (!mongoose.Types.ObjectId.isValid(belongsToClub)) {
+		return res.status(404).json({ error: "Club not found." });
+	}
 
-  let event;
-  // add to database
-  try {
-    const previewBuffer = req.file ? req.file.buffer.toString('base64') : null;
-    const extension = req.file ? `image/${req.file.originalname.split('.').pop()}` : null;
+	const club = await Club.findById(belongsToClub);
 
-    event = await Event.create({
-      title,
-      description,
-      contact,
-      tags,
-      date,
-      time,
-      location,
-      belongsToClub,
-      preview: {
-        data: previewBuffer,
-        extension: extension
-      }
-    });
-    
-    res.status(201).json(event);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+	if (!club) {
+		return res.status(404).json({ error: "Club not found." });
+	}
 
-  // add event to club
-  if (event) {await Club.findByIdAndUpdate(belongsToClub,{ $push: { events: event._id } },{ new: true });}
+	if (club.owner !== req.user._id) {
+		return res.status(401).json({ error: "Unauthorized." });
+	}
+
+	let event;
+
+	try {
+		const previewBuffer = req.file ? req.file.buffer.toString("base64") : null;
+		const extension = req.file
+			? `image/${req.file.originalname.split(".").pop()}`
+			: null;
+
+		event = await Event.create({
+			title,
+			description,
+			contact,
+			tags,
+			date,
+			time,
+			location,
+			belongsToClub,
+			preview: {
+				data: previewBuffer,
+				extension: extension,
+			},
+		});
+
+		res.status(201).json(event);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+
+	// add event to club
+	if (event) {
+		await Club.findByIdAndUpdate(
+			belongsToClub,
+			{ $push: { events: event._id } },
+			{ new: true }
+		);
+	}
 };
 
 // delete an event
 const deleteEvent = async (req, res) => {
-  // fetchs a single event based on id
-  const { id } = req.params;
+	// fetchs a single event based on id
+	const { id } = req.params;
 
-  // if event id is invalid
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "Event not found." });
-  }
+	// if event id is invalid
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "Event not found." });
+	}
 
-  // find the event and delete it by id
-  const event = await Event.findByIdAndDelete(id);
+	const event = await Event.findById(id);
 
-  // if event id does not exist
-  if (!event) {
-    return res.status(404).json({ error: "Event not found." });
-  }
+	// if event id does not exist
+	if (!event) {
+		return res.status(404).json({ error: "Event not found." });
+	}
 
-  // remove the event from the club
-  await Club.findByIdAndUpdate(event.belongsToClub, { $pull: { events: event._id } }, { new: true });
+	const club = await Club.findById(event.belongsToClub);
 
-  res.status(200).json(event);
+	if (club.owner !== req.user._id) {
+		return res.status(401).json({ error: "Unauthorized." });
+	}
+
+	// find the event and delete it by id
+	await Event.findByIdAndDelete(id);
+
+	// remove the event from the club
+	await Club.findByIdAndUpdate(
+		event.belongsToClub,
+		{ $pull: { events: event._id } },
+		{ new: true }
+	);
+
+	res.status(200).json(event);
 };
 
 // update an event
 const updateEvent = async (req, res) => {
-  // fetchs a single event based on id
-  const { id } = req.params;
+	// fetchs a single event based on id
+	const { id } = req.params;
 
-  // if event id is invalid
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "Event not found." });
-  }
+	// if event id is invalid
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "Event not found." });
+	}
 
-  try {
-    const event = await Event.findByIdAndUpdate(id, { ...req.body }, { new: true, runValidators: true });
+	const event = await Event.findById(id);
 
-    // if event id does not exist
-    if (!event) {
-      return res.status(404).json({ error: "Event not found." });
-    }
+	// if event id does not exist
+	if (!event) {
+		return res.status(404).json({ error: "Event not found." });
+	}
 
-    res.status(200).json(event);
-  } catch (error) {
-    if (error instanceof mongoose.Error.ValidationError) {
-      // Handle validation error
-      return res.status(400).json({ error: error.message });
-    }
-  }
+	const club = await Club.findById(event.belongsToClub);
+
+	if (club.owner !== req.user._id) {
+		return res.status(401).json({ error: "Unauthorized." });
+	}
+
+	try {
+		await Event.findByIdAndUpdate(
+			id,
+			{ ...req.body },
+			{ new: true, runValidators: true }
+		);
+
+		res.status(200).json(event);
+	} catch (error) {
+		if (error instanceof mongoose.Error.ValidationError) {
+			// Handle validation error
+			return res.status(400).json({ error: error.message });
+		}
+	}
 };
 
 // exporting all methods
 module.exports = {
-  getAllEvents,
-  getClubEventPreviews,
-  getEvent,
-  createEvent,
-  deleteEvent,
-  updateEvent,
+	getEvents,
+	getEvent,
+	createEvent,
+	deleteEvent,
+	updateEvent,
 };
--- server/routes/events.js
+++ server/routes/events.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
-const { preview, handleUploadError } = require('../helpers/fileHelper')
+const { preview, handleUploadError, customRequireAuth } = require('../helpers/fileHelper');
 
 // Import controllers
 const {
-  getAllEvents,
-  getClubEventPreviews,
+  getEvents,
   getEvent,
   createEvent,
   deleteEvent,
@@ -14,11 +12,10 @@ const {
 } = require("../controllers/eventController");
 
 // API routes for Event Controller
-router.get("/", getAllEvents); // get all events
-router.get("/of-club/:id", getClubEventPreviews); // get all events for a club based on its id
+router.get("/", getEvents); // get all events
 router.get("/:id", getEvent); // get a single event
-router.post("/", preview.single('preview'), handleUploadError, createEvent); // create a new event
-router.delete("/:id", deleteEvent); // delete an existing event
-router.patch("/:id", updateEvent); // update an existing event
+router.post("/", customRequireAuth, preview.single('preview'), handleUploadError, createEvent); // create a new event
+router.delete("/:id", customRequireAuth, deleteEvent); // delete an existing event
+router.patch("/:id", customRequireAuth, updateEvent); // update an existing event
 
 module.exports = router;
