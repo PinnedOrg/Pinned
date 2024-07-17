4cfefc80442b05bafe24532336b3d5e435f0a55b
HazemS24 <hazem.saad2004@gmail.com>
2024-07-17 17:42:26 -0400

    Added puncuation.

--- pinned-react/src/components/error/ClubNotFoundErrorMessage.tsx
+++ pinned-react/src/components/error/ClubNotFoundErrorMessage.tsx
@@ -3,7 +3,7 @@ const ClubNotFoundErrorMessage = () => {
   return (
     <div className="w-full text-center text-muted-foreground">
         <h1 className="text-3xl font-medium ">No clubs found</h1>
-        <p className="mt-2">Please consider broadening your search</p>
+        <p className="mt-2">Please consider broadening your search.</p>
     </div>
   )
 }
