50bd8273640f52d0bcf6b26abec2332b2fb5284b
eshaan-mehta <eshaanm.mehta@gmail.com>
2024-08-24 17:26:58 -0400

    icons for social media

--- pinned-react/src/components/clubprofile/ClubProfileHero.tsx
+++ pinned-react/src/components/clubprofile/ClubProfileHero.tsx
@@ -1,5 +1,11 @@
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa6";
+import { FaDiscord } from "react-icons/fa6";
+import { MdOutlineMailOutline } from "react-icons/md";
+
 import { IClub } from '@/lib/types';
 import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 
 type ClubProfileHeroProps = {
   clubData: IClub
@@ -20,26 +26,31 @@ const ClubProfileHero = ({ clubData }: ClubProfileHeroProps) => {
         <div className="w-full lg:w-1/2 z-10 flex flex-col justify-center p-6 order-2 lg:order-1">
           <h1 className="text-lg font-medium text-purple-600 dark:text-purple-400">WELCOME TO OUR CLUB</h1>
           <h2 className="mt-2 text-4xl lg:text-7xl font-extrabold text-gray-900 dark:text-white">{clubData.name}</h2>
+          <div className="mt-4 flex space-x-4">
+            {clubData.instagram && (
+              <Link target="blank" to={clubData.instagram} className="text-2xl text-gray-600 dark:text-gray-400 hover:text-purple-600 dark:hover:text-white">
+                <FaInstagram />
+              </Link>
+            )}
+            {clubData.discord && (
+              <Link target="blank" to={clubData.discord} className="text-2xl text-gray-600 dark:text-gray-400 hover:text-purple-600 dark:hover:text-white">
+                <FaDiscord />
+              </Link>
+            )}
+            {clubData.email && (
+              <Link target="blank" to={`mailto:${clubData.email}`} className="text-2xl text-gray-600 dark:text-gray-400 hover:text-purple-600 dark:hover:text-white">
+                <MdOutlineMailOutline />
+              </Link>
+            )}
+            {clubData.facebook && (
+              <Link target="blank" to={clubData.facebook} className="text-2xl text-gray-600 dark:text-gray-400 hover:text-purple-600 dark:hover:text-white">
+                <FaFacebook />
+              </Link>
+            )}
+          </div>
           <p className="mt-6 text-base lg:text-lg max-w-md text-gray-700 dark:text-gray-300">
             {clubData.description}
           </p>
-          <div className="mt-4 flex space-x-4">
-            {clubData.facebook && (
-              <a href={clubData.facebook} className="text-gray-600 dark:text-gray-400 hover:text-purple-600 dark:hover:text-white">
-                FB
-              </a>
-            )}
-            {clubData.instagram && (
-              <a href={clubData.instagram} className="text-gray-600 dark:text-gray-400 hover:text-purple-600 dark:hover:text-white">
-                IG
-              </a>
-            )}
-            {clubData.discord && (
-              <a href={clubData.discord} className="text-gray-600 dark:text-gray-400 hover:text-purple-600 dark:hover:text-white">
-                DC
-              </a>
-            )}
-          </div>
         </div>
         <div className={`w-full lg:w-1/3 flex items-center justify-center rounded-lg mt-8 lg:mt-0 order-1 lg:order-2 transition-transform duration-1000 ${
           isVisible ? 'transform translate-x-0' : 'transform translate-x-full'
