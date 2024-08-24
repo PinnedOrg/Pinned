6d08b4a0394d6075642664e6323b56c27686dbc9
eshaan-mehta <eshaanm.mehta@gmail.com>
2024-08-24 16:24:42 -0400

    commented out unused stuff

--- pinned-react/src/pages/ClubProfile.tsx
+++ pinned-react/src/pages/ClubProfile.tsx
@@ -4,22 +4,22 @@ import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react';
 import GradientBackground from '@/components/clubprofile/GradientBackground';
 import ClubProfileHero from '@/components/clubprofile/ClubProfileHero';
-import ClubProfileAboutUs from '@/components/clubprofile/ClubProfileAboutUs';
-import ClubProfileItemizedDescription from '@/components/clubprofile/ClubProfileItemizedDescription';
-import ClubProfilePhotos from '@/components/clubprofile/ClubProfilePhotos';
+// import ClubProfileAboutUs from '@/components/clubprofile/ClubProfileAboutUs';
+// import ClubProfileItemizedDescription from '@/components/clubprofile/ClubProfileItemizedDescription';
+// import ClubProfilePhotos from '@/components/clubprofile/ClubProfilePhotos';
 import ClubDoesNotExistErrorMessage from '@/components/error/ClubDoesNotExistErrorMessage';
 import LoadingSpinner from '@/components/shared/LoadingSpinner';
 import ClubErrorMessage from "@/components/error/ClubErrorMessage";
 import { FAQAccordion } from "@/components/clubprofile/FAQAccordion";
 const ClubProfile = () => {
-  const [hasScrolledAboutUs, setHasScrolledAboutUs] = useState(false);
-  const [hasScrolledFacts, setHasScrolledFacts] = useState(false);
-  const [hasScrolledPhotos, setHasScrolledPhotos] = useState(false);
+  // const [hasScrolledAboutUs, setHasScrolledAboutUs] = useState(false);
+  // const [hasScrolledFacts, setHasScrolledFacts] = useState(false);
+  // const [hasScrolledPhotos, setHasScrolledPhotos] = useState(false);
 
   const { clubId } = useParams()
 
   const fetchClubData = ( clubId: string ) => {
-    return axiosInstance.get(`http://localhost:8080/api/clubs/${clubId}`);
+    return axiosInstance.get(`/api/clubs/${clubId}`);
   }
 
   const {isFetching, isError, data } = useQuery({
@@ -28,26 +28,26 @@ const ClubProfile = () => {
     refetchOnWindowFocus: false,
   });
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      if (scrollPosition > 200) {
-        setHasScrolledAboutUs(true);
-      }
-      if (scrollPosition > 650) {
-        setHasScrolledFacts(true);
-      }
-      if (scrollPosition > 1200) {
-        setHasScrolledPhotos(true);
-      }
-    };
+  // useEffect(() => {
+  //   // const handleScroll = () => {
+  //   //   const scrollPosition = window.scrollY;
+  //   //   if (scrollPosition > 200) {
+  //   //     setHasScrolledAboutUs(true);
+  //   //   }
+  //   //   if (scrollPosition > 650) {
+  //   //     setHasScrolledFacts(true);
+  //   //   }
+  //   //   if (scrollPosition > 1200) {
+  //   //     setHasScrolledPhotos(true);
+  //   //   }
+  //   // };
 
-    window.addEventListener('scroll', handleScroll);
+  //   window.addEventListener('scroll', handleScroll);
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
 
   const clubData = data?.data;
   console.log(clubData);
