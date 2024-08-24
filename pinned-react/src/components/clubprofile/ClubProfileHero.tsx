8fd5b02aaf25b4b14f4e36d8910cfa45b21328ac
eshaan-mehta <eshaanm.mehta@gmail.com>
2024-08-24 18:09:49 -0400

    added imagekit to cllubprofile logo

--- pinned-react/src/components/clubprofile/ClubProfileHero.tsx
+++ pinned-react/src/components/clubprofile/ClubProfileHero.tsx
@@ -6,6 +6,8 @@ import { MdOutlineMailOutline } from "react-icons/md";
 import { IClub } from '@/lib/types';
 import { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
+import { IKContext, IKImage } from "imagekitio-react";
+import { imagekitEndpoints } from "@/lib/data";
 
 type ClubProfileHeroProps = {
   clubData: IClub
@@ -13,6 +15,7 @@ type ClubProfileHeroProps = {
 
 const ClubProfileHero = ({ clubData }: ClubProfileHeroProps) => {
   const [isVisible, setIsVisible] = useState(false);
+  const hasLogo = clubData.logo !== null;
 
   useEffect(() => {
     setIsVisible(true);
@@ -25,7 +28,7 @@ const ClubProfileHero = ({ clubData }: ClubProfileHeroProps) => {
       }`}>
         <div className="w-full lg:w-1/2 z-10 flex flex-col justify-center p-6 order-2 lg:order-1">
           <h1 className="text-lg font-medium text-purple-600 dark:text-purple-400">WELCOME TO OUR CLUB</h1>
-          <h2 className="mt-2 text-4xl lg:text-7xl font-extrabold text-gray-900 dark:text-white">{clubData.name}</h2>
+          <h2 className="mt-2 text-4xl lg:text-7xl font-bold text-gray-900 dark:text-white">{clubData.name}</h2>
           <div className="mt-4 flex space-x-4">
             {clubData.instagram && (
               <Link target="blank" to={clubData.instagram} className="text-2xl text-gray-600 dark:text-gray-400 hover:text-purple-600 dark:hover:text-white">
@@ -55,11 +58,19 @@ const ClubProfileHero = ({ clubData }: ClubProfileHeroProps) => {
         <div className={`w-full lg:w-1/3 flex items-center justify-center rounded-lg mt-8 lg:mt-0 order-1 lg:order-2 transition-transform duration-1000 ${
           isVisible ? 'transform translate-x-0' : 'transform translate-x-full'
         }`}>
-          <img
-            src="https://as2.ftcdn.net/v2/jpg/05/21/15/83/1000_F_521158350_czdCbzL74T78pgPhT7pT4KnDBkldoHvy.jpg"
-            alt="Club"
-            className="w-full h-auto object-cover rounded-lg shadow-lg"
-          />
+          {hasLogo ? (
+                <IKContext urlEndpoint={imagekitEndpoints['club']} publicKey={import.meta.env.IMAGEKIT_PUBLIC_KEY}>
+                  <IKImage 
+                    src={clubData.logo.url}
+                    alt={""} 
+                    loading="lazy" 
+                    lqip={{ active:true, quality:20 }} 
+                    className='overflow-hidden rounded-2xl max-w-[15rem] h-auto object-cover'
+                  />
+                </IKContext>
+              ) : (
+                <img src="/images/logos/LogoPlaceholder.png" alt="placeholder" className='aspect-square rounded-2xl max-w-[15rem] h-auto'/>
+              )}
         </div>
       </div>
     </div>
