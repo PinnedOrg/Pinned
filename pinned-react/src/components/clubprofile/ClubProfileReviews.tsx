c0ec5656d8f9a250d88c57339218352571712544
eshaan-mehta <eshaanm.mehta@gmail.com>
2024-08-24 21:18:31 -0400

    css changes. Added new files for reviews

--- pinned-react/src/components/clubprofile/ClubProfileReviews.tsx
+++ pinned-react/src/components/clubprofile/ClubProfileReviews.tsx
@@ -1,3 +1,23 @@
 
-const ClubProfileReviews = () => {
-    
+import { IReview } from "@/lib/types";
+import { useAuth } from "@clerk/clerk-react";
+import { useState } from "react";
+import AddOrEditReviewModal from "@/components/modals/AddOrEditReviewModal";
+
+type ClubProfileReviewsProps = {
+    reviews: IReview[];
+}
+
+const ClubProfileReviews = ({ reviews }: ClubProfileReviewsProps) => {
+    const { userId } = useAuth();
+    console.log(userId);
+    const userReview = reviews.find((review: IReview) => review.user.clerkId === userId);
+    return (
+        <section className="w-full">
+            <h3 className="text-3xl font-bold mb-8 text-left w-full">Reviews</h3>
+            <AddOrEditReviewModal review={userReview ?? undefined} />
+        </section>
+    )
+}
+
+export default ClubProfileReviews;
---
+++ pinned-react/src/components/modals/AddOrEditReviewModal.tsx
@@ -0,0 +1,53 @@
+import {
+    Dialog,
+    DialogPortal,
+    DialogOverlay,
+    DialogClose,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription,
+} from "@/components/ui/dialog"
+
+import { IReview } from "@/lib/types";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
+import { routes } from "@/lib/routes";
+
+type AddOrEditReviewModalProps = {
+    review: IReview | undefined
+}
+
+
+const AddOrEditReviewModal = ({ review }: AddOrEditReviewModalProps) => {
+    const { userId } = useAuth();
+    const isEditMode = review !== null || review !== undefined;
+
+    return (
+        <Dialog>
+            <DialogTrigger>
+                {userId ? (
+                    <Link to={routes.SignIn}>
+                        <Button variant={'default'}>
+                            Add Review
+                        </Button>
+                    </Link>
+                ) : (
+                    <Button>
+                        {`${isEditMode ? 'Edit' : 'Add'} Review`}
+                    </Button>
+                )}
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default AddOrEditReviewModal;
