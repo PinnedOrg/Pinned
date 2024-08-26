b73e73aa3d176712f92d02e6af3458196f1481ca
eshaan-mehta <eshaanm.mehta@gmail.com>
2024-08-26 16:42:37 -0400

    updated review model and controllers

--- server/models/Review.js
+++ server/models/Review.js
@@ -2,11 +2,35 @@ const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema(
     {
-        rating: {
+        engagement: {
             type: Number,
             required: true,
-            min: [0, "Rating too low."],
-            max: [5, "Rating too high."],
+            min: [0, "Engagement rating too low."],
+            max: [5, "Engagement rating too high."],
+        },
+        commitment: {
+            type: Number,
+            required: true,
+            min: [0, "Commitment rating too low."],
+            max: [5, "Commitment rating too high."],
+        },
+        inclusivity: {
+            type: Number,
+            required: true,
+            min: [0, "Inclusivity rating too low."],
+            max: [5, "Inclusivity rating too high."],
+        },
+        organization: {
+            type: Number,
+            required: true,
+            min: [0, "Organization rating too low."],
+            max: [5, "Organization rating too high."],
+        },
+        comment: {
+            type: String,
+            required: false,
+            trim: true,
+            maxlength: [500, "Comment too long."],
         },
         user: {
             type: mongoose.Schema.Types.ObjectId,
--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { customRequireAuth } = require('../helpers/authHelper');
 
-// Import controllers
 const {
-    addOrUpdateReview
+    addOrUpdateReview,
+    deleteReview
 } = require("../controllers/reviewController");
 
-// API routes for Event Controller
-router.put("/", customRequireAuth, addOrUpdateReview);
+
+router.put("/:clubId", customRequireAuth, addOrUpdateReview);
+router.delete("/:reviewId", customRequireAuth, deleteReview);
 
 module.exports = router;
