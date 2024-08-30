c84ebeaa883f8c3c76822ea4e8bb195e6ec16237
eshaan-mehta <eshaanm.mehta@gmail.com>
2024-08-30 00:01:53 -0400

    added dockerfile and created a frontend build

--- pinned-react/src/index.tsx
+++ pinned-react/src/index.tsx
@@ -8,7 +8,7 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import { ThemeProvider } from "@/components/shared/ThemeProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.CLERK_PUBLISHABLE_KEY
  
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
