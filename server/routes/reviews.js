48a60a868d63aa63c898b94ba746c546b102fba7
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2024-09-03 16:48:38 -0400

    script for adding genres + fix filter bug for genre (#69)
    * add genre to clubs
    
    * fix filter bug
    
    * cleanup
    
    * cleanup

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { customRequireAuth } = require('../helpers/authHelper');
+const clubController = require("../controllers/clubController");
 
 const {
     addOrUpdateReview,
