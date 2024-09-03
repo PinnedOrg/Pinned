7514d0e523d1c32f196d71643f1be6414c5d4926
eshaan-mehta <eshaanm.mehta@gmail.com>
2024-09-02 22:27:38 -0400

    minor changes. Css fixes

--- client/src/components/landing page/featuredclubs.tsx
+++ client/src/components/landing page/featuredclubs.tsx
@@ -19,29 +19,24 @@ const FeaturedClubs = () => {
   });
 
   return (
-    <section className="container flex-wrap justify-center pt-16 gap-y-10" id="featured-clubs-section">
+    <section className="container pt-16 gap-y-10" id="featured-clubs-section">
       <h1 className="w-full text-2xl font-bold text-center lg:text-3xl">Featured Clubs</h1>
       <p className="mt-2 text-center text-muted-foreground">These are our top clubs for this week. Club of the week is the top established club, Upcoming club a new club starting out</p>
-      <div className={`grid grid-cols-1 gap-16 ${(!isError && data?.data.length > 0) ? 'md:grid-cols-2' : ''} min-h-[25rem] `}>
-          {isFetching &&
+      {/* <div className={`grid grid-cols-1 ${(!isError && data?.data.length > 0) ? 'md:grid-cols-2' : ''} min-h-[25rem] mt-4`}> */}
+      <div className={`flex gap-8 justify-center min-h-[25rem] mt-4`}>
+          {isFetching ?
             <>
               <ClubLoadingPlaceholder />
               <ClubLoadingPlaceholder />
             </>
-          }
-          {isError &&
-              <div className='h-full mt-24'>
-                <ClubFetchingErrorMessage />
-              </div>
-          }
-          {isSuccess &&
-              data?.data.length > 0? data.data.map((club: IClub) => (
+          : (isError || data?.data.length === 0) ?
+            <div className='h-full mt-24'>
+              <ClubFetchingErrorMessage />
+            </div>
+          :
+              data?.data.map((club: IClub) => (
                 <ClubPreviewCard club={club} key={club._id}/>
-              )) : (
-                <div className='h-full mt-24'>
-                  <ClubFetchingErrorMessage />
-                </div>
-              )
+              ))
           }
       </div>
 
