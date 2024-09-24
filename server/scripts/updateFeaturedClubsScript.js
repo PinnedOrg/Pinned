bc19658280e1f3b04a62c9bae35895be3b819559
eshaan-mehta <eshaanm.mehta@gmail.com>
2024-09-24 10:54:04 -0400

    updated club of the week scripts

--- server/scripts/updateFeaturedClubsScript.js
+++ server/scripts/updateFeaturedClubsScript.js
@@ -5,6 +5,7 @@ const { connectToDatabase } = require('../database/db');
 const Club = require('../models/Club');
 
 async function updateFeaturedClubs () {
+    const numFeaturedClubs = 2;
     let session;
     try {
         // Connect to the database
@@ -13,30 +14,41 @@ async function updateFeaturedClubs () {
         session = await mongoose.startSession();
         session.startTransaction();
 
-        const oldClubOfTheWeek = await Club.findOne({ featured: 1 }).session(session);
-        const oldUpcomingClub = await Club.findOne({ featured: 2 }).session(session);
+        const oldClubsOfTheWeek = await Club.find({ featured: 1 }).session(session);
+        // const oldUpcomingClub = await Club.findOne({ featured: 2 }).session(session);
 
-        console.log('Old club of the week:', oldClubOfTheWeek.name);
-        console.log('Old upcoming club:', oldUpcomingClub.name);
+        console.log('Old club of the week:', oldClubsOfTheWeek.map(club => club.name));
     
         // Pick a random club to be new club of the week and upcoming club
-        const newClubOfTheWeek = await Club.findOne({ 
-            isActive: true,
-            validation: true,
-            _id: { $nin: [oldClubOfTheWeek._id, oldUpcomingClub._id] },
-            $expr: { $gte: ["$size", 12] } 
-        }).session(session);
+        let newClubsOfTheWeek = [];
+        for (let i = 0; i < numFeaturedClubs; i++) {
+            const club = await Club.findOne({ 
+                isActive: true,
+                validation: true,
+                featured: 0,
+                _id: { $nin: oldClubsOfTheWeek.map(club => club._id) },
+                $expr: { $gte: ["$size", 12] } 
+            }).session(session);
+
+            if (!club) {
+                console.log('No new club of the week found');
+                await session.abortTransaction();
+                session.endSession();
+                mongoose.connection.close();
+                return;
+            }
+            newClubsOfTheWeek.push(club);
+        }
+        // const newUpcomingClub = await Club.findOne({ 
+        //     isActive: true,
+        //     validation: true,
+        //     _id: { $nin: [oldClubOfTheWeek._id, oldUpcomingClub._id] },
+        //     $expr: { $lt: ["$size", 12] } 
+        // }).session(session);
         
-        const newUpcomingClub = await Club.findOne({ 
-            isActive: true,
-            validation: true,
-            _id: { $nin: [oldClubOfTheWeek._id, oldUpcomingClub._id] },
-            $expr: { $lt: ["$size", 12] } 
-        }).session(session);
-        
-        if (!newClubOfTheWeek) console.log('No new club of the week found'); 
-        if (!newUpcomingClub) console.log('No new upcoming club found');
-        if (!newClubOfTheWeek || !newUpcomingClub) {
+        if (newClubsOfTheWeek.length === 0) console.log('No new club of the week found'); 
+        // if (!newUpcomingClub) console.log('No new upcoming club found');
+        if (newClubsOfTheWeek.length === 0 || !newUpcomingClub) {
             await session.abortTransaction();
             session.endSession();
             mongoose.connection.close();
@@ -44,18 +56,20 @@ async function updateFeaturedClubs () {
         }
 
         await Club.updateMany({}, { featured: 0 }).session(session);
-        await Club.findByIdAndUpdate(newClubOfTheWeek._id, { featured: 1 }).session(session);
-        await Club.findByIdAndUpdate(newUpcomingClub._id, { featured: 2 }).session(session);
+        foreach(newClubsOfTheWeek, async (club) => {
+            await Club.findByIdAndUpdate(club._id, { featured: 1 }).session(session);
+        });
+        // await Club.findByIdAndUpdate(newUpcomingClub._id, { featured: 2 }).session(session);
     
-        console.log('Club of the week updated from ', oldClubOfTheWeek.name, ' to ', newClubOfTheWeek.name);
-        console.log('Upcoming club updated from ', oldUpcomingClub.name, ' to ', newUpcomingClub.name);
+        // console.log('Club of the week updated from ', oldClubOfTheWeek.name, ' to ', newClubOfTheWeek.name);
+        // console.log('Upcoming club updated from ', oldUpcomingClub.name, ' to ', newUpcomingClub.name);
         
         await session.commitTransaction();
         session.endSession();
         mongoose.connection.close();
     } catch (error) {
-        await session.abortTransaction();
-        session.endSession();
+        await session?.abortTransaction();
+        session?.endSession();
         mongoose.connection.close();
         console.error('Error updating club of the week or upcoming club:', error);
     }
